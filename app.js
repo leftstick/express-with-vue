a17fc4a02840df50b1a9f9085fd2d7813f534f2f
Howard.Zuo <leftstick@qq.com>
2019-04-19 13:53:04 +0800

    refactor: use express-api-loader instead

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const { withExpressApp } = require('express-api-loader')
 
 const { initIndexPageEntry } = require('./server/setup/entry')
 const { initStaticAssetsHandler } = require('./server/setup/staticAssets')
 const { initRequestBodyParser } = require('./server/setup/requestBody')
 const { initCookieParser } = require('./server/setup/cookie')
-const { registerAPIs } = require('./server/setup/apis')
 const { initServerRunner } = require('./server/setup/serverRunner')
+
+const { resolveFromRoot } = require('./server/assistant/utils/path')
 
 const app = express()
 
@@ -13,7 +15,14 @@ initStaticAssetsHandler(app)
 initRequestBodyParser(app)
 initCookieParser(app)
 
-registerAPIs(app)
+withExpressApp(app)({
+  scanOpts: {
+    cwd: resolveFromRoot('server/apis'),
+    pattern: '**/*.js',
+    ignore: ['**/_*.js', '**/_*/*.js']
+  },
+  apiPrefix: '/apis'
+})
 
 initIndexPageEntry(app)
 
--- client/common/polyfill/index.js
+++ client/common/polyfill/index.js
@@ -1,16 +1,16 @@
-import 'core-js/fn/array/find'
-import 'core-js/fn/array/includes'
-import 'core-js/fn/array/find-index'
+import 'core-js/features/array/find'
+import 'core-js/features/array/includes'
+import 'core-js/features/array/find-index'
 
-import 'core-js/fn/symbol/for'
-import 'core-js/fn/symbol/key-for'
-import 'core-js/fn/array/virtual/iterator'
+import 'core-js/features/symbol/for'
+import 'core-js/features/symbol/key-for'
+import 'core-js/features/array/virtual/iterator'
 
-import 'core-js/fn/function/name'
+import 'core-js/features/function/name'
 
-import 'core-js/fn/object/assign'
+import 'core-js/features/object/assign'
 
-import 'core-js/fn/promise'
+import 'core-js/features/promise'
 
-import 'core-js/fn/string/includes'
-import 'core-js/fn/string/starts-with'
+import 'core-js/features/string/includes'
+import 'core-js/features/string/starts-with'
